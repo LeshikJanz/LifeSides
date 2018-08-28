c2f96f83906b4c306d19dcd82dbfb822aebcfc64
leshikjanz <leshikjanz@zivitech.com>
2018-08-28 20:58:01 +0300

    feat: implement dropdown

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,22 +1,59 @@
 // @flow
 
-import React from 'react'
+import React, { Fragment } from 'react'
 import './header.scss'
 import { MONTH_NAMES_COUNTED } from 'constants/dates'
 import userIcon from 'assets/images/user-icon.png'
+import Dropdown from '../dropdown/Dropdown'
+import { HEADER_MENU_ITEMS } from './constants'
 
 class Header extends React.Component<{}> {
+  state = {
+    isDropdownOpen: false,
+  }
+
+  handleOpen = () => {
+    this.setState({ isDropdownOpen: true })
+  }
+
+  onDropdownChange = () => {
+
+  }
+
+  onMouseLeave = () => {
+    if (this.state.isDropdownOpen) {
+      this.setState({ isDropdownOpen: false })
+    }
+  }
+
   render () {
     const date = new Date()
+    const { isDropdownOpen } = this.state
     return (
       <div className="header-wrapper">
         <div className="header-logo-label">
           Life Sides
         </div>
-        <div className="header-logo-container">
-          <button className="green">
+        <div className="header-logo-container" onMouseLeave={this.onMouseLeave}>
+          <button className="green" onMouseOver={this.handleOpen}>
             <span>Создать</span>
           </button>
+          {
+            isDropdownOpen &&
+            <ul className="header-dropdown">
+              {
+                HEADER_MENU_ITEMS.map((item, index) =>
+                  <li
+                    key={index}
+                    onClick={() => this.onDropdownChange(item)}
+                    value={item.value}
+                  >
+                    {item.title}
+                  </li>
+                )
+              }
+            </ul>
+          }
           <div className="header-date">
             {`${date.getDate()} ${MONTH_NAMES_COUNTED[date.getMonth()]} ${date.getFullYear()}`}
           </div>
---
+++ client/src/components/header/constants.js
@@ -0,0 +1,14 @@
+// @flow
+
+import type { DropdownItem } from 'types/common'
+
+export const HEADER_MENU_ITEMS: DropdownItem[] = [
+  {
+    title: "Сторону жизни",
+    value: "lifeside",
+  },
+  {
+    title: "Привычку",
+    value: "habbit",
+  }
+]
